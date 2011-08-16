460b63e1bee3f200e3e78e883c132336dad13a05
Daniel Robbins <drobbins@funtoo.org>
2011-08-16 11:45:38 -0600

    attempted firmware building fix - go back to gentoo-style detection but disable if firmware_install target doesn't exist in Makefile

--- gen_compile.sh
+++ gen_compile.sh
@@ -310,6 +310,28 @@ compile_kernel() {
 		compile_generic "${KERNEL_MAKE_DIRECTIVE_2}" kernel
 	fi
 
+	local ext_fw_build="yes"
+
+	local fw_makefile='fgrep firmware_install "${BUILD_SRC}"/Makefile'
+	if [ -z "${fw_makefile}" ]
+	then
+		print_info 1 "        >> Disabling firmware build due to firmware_install target not being available..."
+		ext_fw_build="no"
+	else
+		local firmware_in_kernel_line=`fgrep CONFIG_FIRMWARE_IN_KERNEL "${BUILD_DST}"/.config`
+		if [ -n "${firmware_in_kernel_line}" -a "${firmware_in_kernel_line}" == CONFIG_FIRMWARE_IN_KERNEL=y ]
+		then
+			ext_fw_build="no"
+			print_info 1 "        >> Not installing firmware as it's included in the kernel already (CONFIG_FIRMWARE_IN_KERNEL=y)..."
+		fi
+	fi
+
+	if [ "$ext_fw_build" == "yes" ]
+	then
+			print_info 1 "        >> Installing firmware ('make firmware_install')..."
+			compile_generic "firmware_install" kernel
+	fi
+
 	# workaround for bug #244651 fix not being very good for RHEL:
 	print_info 1 "        >> Installing firmware ('make firmware_install')..."
 	compile_generic "firmware_install" kernel
