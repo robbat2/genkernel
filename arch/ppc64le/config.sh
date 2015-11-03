7d6671d55d06082641987fae6e24903a1990de57
Kevin Zhao <kevin.zhaoshuai@cn.ibm.com>
2015-11-03 03:25:40 -0500

    ppc64le support
    Bug fix:Bug 564752 - sys-kernel/genkernel : ERROR: ppc64le not yet supported by genkernel.Please add the arch-specific config file
    
    Add support of genkernel for arch ppc64le.Then Gentoo profiles has
    supportted ppc64le now.Add a new arch configuration ,kernel configure
    and module loading file.Also add ppc64le to  gen_arch.sh.
    
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>
    Fixes: https://bugs.gentoo.org/564752

---
+++ arch/ppc64le/config.sh
@@ -0,0 +1,26 @@
+# $Id$
+#
+# Arch-specific options that normally shouldn't be changed.
+#
+KERNEL_MAKE_DIRECTIVE="vmlinux"
+KERNEL_BINARY="vmlinux"
+
+#
+# Arch-specific defaults that can be overridden in the config file or on the
+# command line.
+#
+DEFAULT_COMPRESS_INITRD=yes
+DEFAULT_COMPRESS_INITRD_TYPE=best
+
+DEFAULT_MAKEOPTS="-j2"
+
+DEFAULT_KERNEL_MAKE=make
+DEFAULT_UTILS_MAKE=make
+
+DEFAULT_KERNEL_CC=gcc
+DEFAULT_KERNEL_AS=as
+DEFAULT_KERNEL_LD=ld
+
+DEFAULT_UTILS_CC=gcc
+DEFAULT_UTILS_AS=as
+DEFAULT_UTILS_LD=ld
