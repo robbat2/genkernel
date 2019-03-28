fe729696c3814267d039d2aad23888acf80b9ede
Thomas Deutschmann <whissi@gentoo.org>
2019-03-28 22:38:28 +0100

    compile_gen_init_cpio(): use 'mkdir -p' when creating $KERNEL_OUTPUTDIR/usr
    This is usually only required for users using genkernel to create initramfs without
    building kernel but it doesn't hurt.
    
    Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

--- gen_compile.sh
+++ gen_compile.sh
@@ -251,7 +251,7 @@ compile_gen_init_cpio() {
 	print_info 1 "$(getIndent 2)>> Compiling gen_init_cpio..."
 
 	[ ! -e "${gen_init_cpio_SRC}" ] && gen_die "'${gen_init_cpio_SRC}' is missing. Cannot compile gen_init_cpio!"
-	[ ! -d "${gen_init_cpio_DIR}" ] && mkdir "${gen_init_cpio_DIR}"
+	[ ! -d "${gen_init_cpio_DIR}" ] && mkdir -p "${gen_init_cpio_DIR}" || gen_die "failed to create '${gen_init_cpio_DIR}'!"
 
 	export_utils_args
 
