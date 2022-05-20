8c9de489290dc470e30f8c7d0aaa3456eb124537
Andreas K. Hüttel <dilfridge@gentoo.org>
2022-05-20 20:52:19 +0200

    arch: Copy s390 config to s390x (it's 64bit anyway!)
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

---
+++ arch/s390x/config.sh
@@ -0,0 +1,8 @@
+# $Id$
+#
+# This file is sourced AFTER defaults/config.sh; generic options should be set there.
+# Arch-specific options that normally shouldn't be changed.
+#
+KERNEL_MAKE_DIRECTIVE=""
+KERNEL_MAKE_DIRECTIVE_2=""
+KERNEL_BINARY="arch/s390/boot/bzImage"
